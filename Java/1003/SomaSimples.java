c765113bcb655f749790a4adebafcfcd493e9bbe
LuisR <rabeloluissouza@gmail.com>
2024-04-08 21:09:23 -0300

    alterando nome da variável

--- Java/1003/SomaSimples.java
+++ Java/1003/SomaSimples.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class SomaSimples{
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
+        int valorA = input.nextInt();
         int b = input.nextInt();
         input.close();
-        int soma = a+b;
+        int soma = valorA+b;
         System.out.print("SOMA = "+soma);
     }
 }
